3a495c40a2e6473e33a36c0bc388925823031cc3
Eissanoor <eissaanoor@gmail.com>
2024-07-06 20:12:49 +0500

    dddd

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRoutes);
 app.get("/home",(req,res) =>{
-  res.json("success154",)
+  res.json("success1540000000",)
 })
 const PORT = process.env.PORT || 3123;
 app.listen(PORT, () => {
